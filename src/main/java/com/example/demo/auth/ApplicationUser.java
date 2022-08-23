55a3a19ca4dbeae149088a1e36392820797de22d
Kiril <kiril_ganev_genchev@abv.bg>
2022-08-23 14:51:59 +0300

    Auth Provider

--- src/main/java/com/example/demo/auth/ApplicationUser.java
+++ src/main/java/com/example/demo/auth/ApplicationUser.java
@@ -5,19 +5,20 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 public class ApplicationUser implements UserDetails {
 
     private final String username;
     private final String password;
-    private final List<? extends GrantedAuthority> grantedAuthorities;
+    private final Set<? extends GrantedAuthority> grantedAuthorities;
     private final boolean isAccountNonExpired;
     private final boolean isAccountNonLocked;
     private final boolean isCredentialsNonExpired;
     private final boolean isEnabled;
 
     public ApplicationUser(String username,
-                           String password,List<? extends GrantedAuthority> grantedAuthorities,
+                           String password, Set<? extends GrantedAuthority> grantedAuthorities,
                            boolean isAccountNonExpired,
                            boolean isAccountNonLocked,
                            boolean isCredentialsNonExpired,
--- src/main/java/com/example/demo/auth/FakeApplicationUserDaoService.java
+++ src/main/java/com/example/demo/auth/FakeApplicationUserDaoService.java
@@ -1,10 +1,64 @@
 package com.example.demo.auth;
 
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
+import static com.example.demo.Security.ApplicationUserRole.*;
+
+@Repository("fake")
 public class FakeApplicationUserDaoService implements ApplicationUserDao{
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public FakeApplicationUserDaoService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
-        return Optional.empty();
+        return getApplicationUsers()
+                .stream()
+                .filter(applicationUser -> username.equals(applicationUser.getUsername()))
+                .findFirst();
+    }
+
+    private List<ApplicationUser> getApplicationUsers(){
+        List<ApplicationUser> applicationUsers = Lists.newArrayList(
+            new ApplicationUser(
+                "Steven",
+                    passwordEncoder.encode("password"),
+                    STUDENT.getGrantedAuthorities(),
+                    true,
+                    true,
+                    true,
+                    true
+            ),
+                new ApplicationUser(
+                        "Tom",
+                        passwordEncoder.encode("password"),
+                        ADMIN.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        "Thomas",
+                        passwordEncoder.encode("password"),
+                        ADMINTRAINEE.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+
+        return applicationUsers;
     }
 }
