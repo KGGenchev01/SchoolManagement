0e8e2b5cf71554dfa8a04468ae5fe2c1b43d4cee
Kiril <kiril_ganev_genchev@abv.bg>
2022-08-23 13:50:51 +0300

    Added users to the application

--- src/main/java/com/example/demo/Security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/Security/ApplicationSecurityConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebSecurity
@@ -28,19 +31,33 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login","index","/css/*","js/*")
+                .antMatchers("/login", "index", "/css/*", "js/*")
                 .permitAll()
-                .antMatchers("/api/**").hasAnyRole(ApplicationUserRole.ADMIN.name(),ApplicationUserRole.STUDENT.name())
+                .antMatchers("/api/**").hasAnyRole(ApplicationUserRole.ADMIN.name(), ApplicationUserRole.STUDENT.name())
 //                .antMatchers(HttpMethod.DELETE,"/management/**").hasAuthority(ApplicationUserPermission.Course_WRITE.getPermission())
 //                .antMatchers(HttpMethod.POST,"/management/**").hasAuthority(ApplicationUserPermission.Course_WRITE.getPermission())
 //                .antMatchers(HttpMethod.PUT,"/management/**").hasAuthority(ApplicationUserPermission.Course_WRITE.getPermission())
 //                .antMatchers("/management/**").hasRole(ApplicationUserRole.ADMIN.name())
-                .antMatchers("/management/**").hasAnyRole(ApplicationUserRole.ADMIN.name(),ApplicationUserRole.ADMINTRAINEE.name())
+                .antMatchers("/management/**").hasAnyRole(ApplicationUserRole.ADMIN.name(), ApplicationUserRole.ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/courses", true)
+                .and()
+                .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(20))
+                .key("SomethingSecured")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/login");
     }
+
     @Override
     @Bean
     protected UserDetailsService userDetailsService(){
