b22703cc5f9db4ec92fd94ba0f4a532f9d1da006
Kiril <kiril_ganev_genchev@abv.bg>
2022-09-21 17:10:19 +0300

    Jwt Token Verifier Filter

--- src/main/java/com/example/demo/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/example/demo/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -62,6 +62,6 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
                 .signWith(Keys.hmacShaKeyFor(key.getBytes()))
                 .compact();
 
-        response.addHeader("Authorization", "Bearer" + token);
+        response.addHeader("Authorization", "Bearer " + token);
     }
 }
